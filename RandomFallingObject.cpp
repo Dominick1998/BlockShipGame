5e247312fa293ebef0a0933a4854a8cd3ddf3d45
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-05-14 23:09:11 -0500

    Create RandomFallingObject.cpp

---
+++ RandomFallingObject.cpp
@@ -0,0 +1,28 @@
+#include "RandomFallingObject.h"
+#include "Ship.h"
+
+using namespace std;
+
+/*
+RandomFallingObject::RandomFallingObject(): Object()
+{
+// Copy code from Block
+}
+
+Vector2D RandomFallingObject::getVelocity() const
+{
+return velocity;
+}
+
+bool RandomFallingObject::collides(const Ship & player)
+{
+// Copy code from Block
+}
+
+void RandomFallingObject::colorize()
+{
+normal_distribution<double> colorChange(0.0, 10.0);
+
+// Write colorize() method
+}
+*/
