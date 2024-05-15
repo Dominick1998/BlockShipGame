5fe76004a54a2fc6da006e566d1129e16152c1f2
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-05-14 23:08:11 -0500

    Create Object.cpp

---
+++ Object.cpp
@@ -0,0 +1,25 @@
+#include "Object.h"
+/*
+std::random_device Object::seed;
+std::default_random_engine Object::objectEngine(Object::seed());
+Object::Object(): sprite({ {0,0}, {0,0,0}, {0,0,0} })
+{
+}
+Object::Object(Sprite sprite): sprite(sprite)
+{}
+
+RGB Object::getColor() const
+{
+return sprite.color;
+}
+
+Vector2D Object::getPosition() const
+{
+return sprite.position;
+}
+
+Dimensions Object::getDimensions() const
+{
+return sprite.dimensions;
+}
+*/
